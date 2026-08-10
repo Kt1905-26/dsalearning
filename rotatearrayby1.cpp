1c2d189c5e165e89417bcaea5d27d8afacc3b856
Kt1905-26 <khanak.24b15310205@abes.ac.in>
2026-08-10 11:22:55 +0530

    push

---
+++ rotatearrayby1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    
+    vector<int>v;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+   int temp=v[0];
+   for(int i=1;i<n;i++){
+    v[i-1]=v[i];
+   }
+   v[n-1]=temp;
+   for(inti=0;i<n;i++){
+    cout<<v[i]<<"";
+
+   }
+   
+}
