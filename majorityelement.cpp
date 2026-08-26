1950ec3870b8af2b44c861ab0d29da29e97426f2
Kt1905-26 <khanak.24b15310205@abes.ac.in>
2026-08-26 11:30:23 +0530

    push

--- majorityelement.cpp
+++ majorityelement.cpp
@@ -1,31 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
 using namespace std;
 
-int main()
-{
-    int n;
-    cin >> n;
+int findMajorityElement(vector<int>& arr) {
+    unordered_map<int, int> freq;
+    int n = arr.size();
 
-    vector<int> arr(n);
-
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    sort(arr.begin(), arr.end());
-
-    int candidate = arr[n / 2];
-    int count = 0;
-
-    for(int i = 0; i < n; i++)
-    {
-        if(arr[i] == candidate)
-            count++;
+    // Count frequencies
+    for (int num : arr) {
+        freq[num]++;
     }
 
-    if(count > n / 2)
-        cout << "Majority Element = " << candidate << endl;
+    // Check majority
+    for (auto it : freq) {
+        if (it.second > n / 2) {
+            return it.first;
+        }
+    }
+
+    return -1; // No majority element
+}
+
+int main() {
+    vector<int> arr = {3, 3, 4, 2, 3, 3, 5};
+    int majority = findMajorityElement(arr);
+
+    if (majority != -1)
+        cout << "Majority element is: " << majority << endl;
     else
-        cout << "No Majority Element" << endl;
+        cout << "No majority element found" << endl;
 
     return 0;
 }
