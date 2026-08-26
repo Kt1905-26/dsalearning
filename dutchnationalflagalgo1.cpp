f54e01a42e7e255419af0c27be1e56b014af3f1e
Kt1905-26 <khanak.24b15310205@abes.ac.in>
2026-08-26 10:58:05 +0530

    push

---
+++ dutchnationalflagalgo1.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v;
+
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+
+    }
+    int low=0;
+    int mid=0;
+    int high=n-1;
+    while(mid<=high){
+        if(v[mid]==0){
+            swap(v[low],v[mid])
+            low++;
+            mid++;
+        }
+        else if(v[mid]==1){
+            mid++;
+        }
+        else{
+            swap(v[mid],v[high]);
+            high--;
+
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+}
